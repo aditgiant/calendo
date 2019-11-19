fce83ce387c44bbb9a929aa8d292b74cc2b8bfc5
Ria <riaratnasari@outlook.com>
2019-11-19 19:04:08 +0100

    list task

--- app/src/main/java/com/example/calendo/MainActivity.java
+++ app/src/main/java/com/example/calendo/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.calendo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
@@ -22,11 +32,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private NavigationView navigationView;
+    ListView listView;
+    String[] mTitle = {"Laundry","Homework","Group Meeting","Shopping", "Dating", "Assignment HCI Seminar"};
+    String [] mDescription ={"ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            "ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            "ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+            "ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  "ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  "ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription);
+        listView.setAdapter(adapter);
 
         //Link UI elements
         fab = findViewById(R.id.fab);
@@ -43,6 +66,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         //Make the toggle rotating
         actionBarDrawerToggle.syncState();
+
+        listView.setItemsCanFocus(true);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Position" + position, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
 
     }
 
@@ -64,6 +98,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (id == R.id.calendar_button) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new CalendarFragment()).commit();
+            listView.setVisibility(View.GONE);
+
             return true;
         }
 
@@ -78,10 +114,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_statistics:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new StatisticsFragment()).commit();
+                listView.setVisibility(View.GONE);
+
                 break;
             case R.id.nav_account:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new AccountFragment()).commit();
+                listView.setVisibility(View.GONE);
+
                 break;
         }
 
@@ -102,5 +142,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Intent intent = new Intent (MainActivity.this, AddNewTask.class);
         startActivity(intent);
 
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        String rDescription[];
+        int rImgs[];
+
+        MyAdapter (Context c, String title[], String description[]) {
+            super(c, R.layout.row, R.id.titleTodo, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+
+            TextView myTitle = row.findViewById(R.id.titleTodo);
+            TextView myDescription = row.findViewById(R.id.descriptionTodo);
+
+            myTitle.setText(rTitle[position]);
+            myDescription.setText(rDescription[position]);
+
+
+
+
+            return row;
+        }
     }
 }
