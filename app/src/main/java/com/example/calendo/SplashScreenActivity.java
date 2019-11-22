e556a06f2ee951d84fa1c23a3830f9ab96a609d3
Ria <riaratnasari@outlook.com>
2019-11-22 17:44:42 +0100

    add splash screen

---
+++ app/src/main/java/com/example/calendo/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.calendo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 2000);
+    }
+}
